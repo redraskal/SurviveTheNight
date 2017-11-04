60a90d70ec92311f5b4acf64145020e381c9a076
redraskal <redraskal@users.noreply.github.com>
2017-11-03 23:06:32 -0500

    Create project structure

---
+++ src/main/java/me/redraskal/survivethenight/SurviveTheNight.java
@@ -0,0 +1,16 @@
+package me.redraskal.survivethenight;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Copyright (c) Redraskal 2017.
+ * <p>
+ * Please do not copy the code below unless you
+ * have permission to do so from me.
+ */
+public class SurviveTheNight extends JavaPlugin {
+
+    public void onEnable() {
+        //TODO
+    }
+}
